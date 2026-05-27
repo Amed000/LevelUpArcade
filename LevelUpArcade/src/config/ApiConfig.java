3a7dde7740c58566a6017201660cf81e7d2720c4
Ahmed Salem <ahmedsalem.gharaga@campusfp.es>
2026-05-27 13:13:19 +0200

    Update ApiConfig.java

--- LevelUpArcade/src/config/ApiConfig.java
+++ LevelUpArcade/src/config/ApiConfig.java
@@ -2,5 +2,5 @@ package config;
 
 public class ApiConfig {
 
-    public static final String API_KEY = "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";
+    public static final String API_KEY = "s";
 }
